380290c55cf1d62abe7d3a97d9362e28909cbeea
Fabian Vogler <fabian@equivalence.ch>
2007-03-28 16:24:50 +0000

    Added new module "storage" to store objects.

---
+++ modules/ch.anomey.storage/module.php
@@ -0,0 +1,51 @@
+<?php
+
+interface Storable {
+
+}
+
+class XMLStoreage implements Storable {
+
+	private $file;
+
+	private $objects = array();
+
+	public function __construct($file) {
+		$this->file = $file;
+	}
+
+	public function store(Storable $object) {
+		$id = spl_object_hash($object);
+		$this->objects[$id] = $object;
+		$this->save();
+	}
+
+	/**
+	 * Adds object to internal memory to be saved.
+	 */
+	public function prepare($object) {
+		$id = spl_object_hash($object);
+		$this->objects[$id] = $object;
+	}
+
+	public function search($query) {
+		$matches = array();
+		foreach ($this->objects as $object) {
+			if(eval('return ' . $query . ';')) {
+				$matches[] = $object;
+			}
+		}
+		return $matches;
+	}
+
+	/**
+	 * Saves objects in internal memory.
+	 */
+	public function save() {
+		foreach ($this->objects as $object) {
+			
+		}
+	}
+}
+
+?>
